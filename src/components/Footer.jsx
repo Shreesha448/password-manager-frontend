8ba49aae04821858fffbb314360685799d88610b
Shreesha448 <kvshreesha2310@gmail.com>
2025-11-25 15:05:16 +0530

    changes to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
             <div className="logo font-bold text-white text-2xl">
-                <span className='text-green-500'> &lt;</span>
-                <span>Password -</span><span className='text-green-500'>Manager/&gt;</span>
+                <span className='text-green-500'></span>
+                <span>Password - Manager</span><span className='text-green-500'> </span>
             </div>
             <div className='flex justify-center items-center'> Created with <img className='w-7 mx-2' src="icons/heart.png" alt="" /> by Shreesha K V</div>
         </div>
